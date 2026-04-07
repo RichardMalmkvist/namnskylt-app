22a2fe7b2dfc10ec39bb4a3c55de090c62c49baf
RichardMalmkvist <richard.malmkvist@jonkopingsskyltfabrik.se>
2026-04-07 14:29:46 +0200

    Update server.js

--- server/server.js
+++ server/server.js
@@ -215,12 +215,6 @@ app.delete("/api/orders/:id", (req, res) => {
 });
 
 // Servera React-appen i produktion
-app.use(express.static(path.join(__dirname, "../build")));
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../build", "index.html"));
-});
-
-// Servera React-appen i produktion
 app.use(express.static(path.join(__dirname, "build")));
 
 app.get("*", (req, res) => {
