3bfb67084d014aa8d185a44f0b8bdf8641fc29e7
RichardMalmkvist <richard.malmkvist@jonkopingsskyltfabrik.se>
2026-04-07 14:31:02 +0200

    Update server.js
    rad 9, uppdaterat till port 8080

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const crypto = require("crypto");
 const nodemailer = require("nodemailer");
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 app.use(cors());
 app.use(express.json());
